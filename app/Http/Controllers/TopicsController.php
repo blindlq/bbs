883f23bf78654ddc9d8d3afa0529d3b722db9206
vagrant <vagrant@homestead>
2018-03-23 10:00:45 +0000

    User can create topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -6,6 +6,8 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Models\Category;
+use Auth;
 
 class TopicsController extends Controller
 {
@@ -22,19 +24,28 @@ class TopicsController extends Controller
 		return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
+    public function show(TopicRequest $request,Topic $topic)
     {
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+
         return view('topics.show', compact('topic'));
     }
 
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
+            $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
-	public function store(TopicRequest $request)
+	public function store(TopicRequest $request,Topic $topic)
 	{
-		$topic = Topic::create($request->all());
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+		//$topic = Topic::create($request->all());
+
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
 	}
 
