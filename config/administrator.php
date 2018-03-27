2b27485af6d2d7e6cfb0b8e0e1fc01d5452347c7
vagrant <vagrant@homestead>
2018-03-27 08:36:27 +0000

    Admin can manage content

--- config/administrator.php
+++ config/administrator.php
@@ -64,6 +64,11 @@ return array(
             'roles',
             'permissions',
         ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
     ],
 
     /*
