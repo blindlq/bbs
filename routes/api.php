c55d1ceea4fa9890714ea17eb07333b7fb8a192f
vagrant <vagrant@homestead>
2018-03-31 06:56:54 +0000

    user can register using phone

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ $api->version('v1',[
     // 短信验证码
     $api->post('verificationCodes', 'VerificationCodesController@store')
         ->name('api.verificationCodes.store');
+    //用户注册
+    $api->post('users','UsersController@store')
+        ->name('api.users.store');
 });
 
 //$api->version('v1', function($api) {
