dc4360148a3d3712cf018184dd65b05f388d3716
vagrant <vagrant@homestead>
2018-04-03 09:21:28 +0000

    add jwt

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use App\Models\User;
-
+use App\Http\Requests\Api\AuthorizationRequest;
+use Auth;
 
 class AuthorizationsController extends Controller
 {
@@ -37,13 +38,9 @@ class AuthorizationsController extends Controller
 
         switch ($type){
             case 'weixin':
-                $unionid = $oauthUser->offsetExists('unionid') ? $oauthUser->offsetGet('unionid') : null;
+                $user = User::where('weixin_unionid', $oauthUser->offsetGet('unionid'))->first();
 
-                if($unionid){
-                    $user = User::where('weixin_unionid', $unionid)->first();
-                }else{
-                    $user = User::where('weixin_openid', $oauthUser->getId())->first();
-                }
+
 
             // 没有用户，默认创建一个用户
             if(!$user){
@@ -51,13 +48,54 @@ class AuthorizationsController extends Controller
                         'name' => $oauthUser->getNickname(),
                         'avatar' => $oauthUser->getAvatar(),
                         'weixin_openid' => $oauthUser->getId(),
-                        'weixin_unionid' => $unionid,
+                        'weixin_unionid' =>$oauthUser->offsetGet('unionid'),
                     ]);
             }
 
                 break;
         }
 
-        return $this->response->array(['token' => $user->id]);
+        $token = Auth::guard('api')->fromUser($user);
+        return $this->respondWithToken($token)->setStatusCode(201);
+
+    }
+
+    public function store(AuthorizationRequest $request)
+    {
+        $username = $request->username;
+        //过滤字段
+        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+
+        $credentials['password'] = $request->password;
+
+
+        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+            return $this->response->errorUnauthorized('用户名或密码错误');
+        }
+
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    public function update()
+    {
+        $token = Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        Auth::guard('api')->logout();
+        return $this->response->noContent();
+    }
+
+    protected function respondWithToken($token)
+    {
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Auth;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasRoles;
 
@@ -112,6 +113,18 @@ class User extends Authenticatable
         }
 
         $this->attributes['avatar'] = $path;
+    }
+
+    // Rest omitted for brevity
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 
 }
