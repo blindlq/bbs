152a2ea6d0d4d2a2f126a2b7d3b175f1cf43af28
vagrant <vagrant@homestead>
2018-04-07 05:34:21 +0000

    api/user

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\Api\UserRequest;
+use App\Transformers\UserTransformer;
 
 class UsersController extends Controller
 {
@@ -32,7 +33,19 @@ class UsersController extends Controller
         //清除验证码缓存
         \Cache::forget($request->verification_key);
 
-        return $this->response->created();
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
+        //return $this->response->created();
 
+    }
+
+    public function me()
+    {
+        return $this->response->item($this->user(),new UserTransformer());
     }
 }
