a1f92ca09da1204534904afd571658a4761fdab9
vagrant <vagrant@homestead>
2018-04-09 08:56:42 +0000

    notifications read

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -18,8 +18,14 @@ class NotificationsController extends Controller
     public function stats()
     {
         return $this->response->array([
-            'unread_count' => $this->user->notification_count,
+            'unread_count' => $this->user()->notification_count,
         ]);
+    }
+    public function read()
+    {
+        $this->user()->markAsRead();
+
+        return $this->response->noContent();
     }
 
 }
