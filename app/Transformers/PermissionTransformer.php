982215abf51730d5b4a02a195f554dbb2bbeee25
vagrant <vagrant@homestead>
2018-04-09 09:26:06 +0000

    permissions index

---
+++ app/Transformers/PermissionTransformer.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Transformers;
+
+use Spatie\Permission\Models\Permission;
+use League\Fractal\TransformerAbstract;
+
+class PermissionTransformer extends TransformerAbstract
+{
+    public function transform(Permission $permission)
+    {
+        return [
+            'id' => $permission->id,
+            'name' => $permission->name,
+        ];
+    }
+}
