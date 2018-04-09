0b79edbdd5244216ae9a350ad448d1ef505705df
vagrant <vagrant@homestead>
2018-04-09 07:41:24 +0000

    replies destroy

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Topic;
 use App\Models\Reply;
-
+use App\Models\User;
 use App\Http\Requests\Api\ReplyRequest;
 use App\Transformers\ReplyTransformer;
 
@@ -21,5 +21,19 @@ class RepliesController extends Controller
 
         return $this->response->item($reply, new ReplyTransformer())
             ->setStatusCode(201);
+    }
+
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        if ($reply->topic_id != $topic->id) {
+            return $this->response->errorBadRequest();
+        }
+        if(($this->user()->id == $reply->user_id) || ($this->user()->id == $topic->user_id)){
+            $reply->delete();
+            return $this->response->noContent();
+        }
+        $this->authorize('destroy', $reply);
+
+
     }
 }
